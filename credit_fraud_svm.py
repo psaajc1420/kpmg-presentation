0ac2d9f73364ef7819705de6d54c946bb8cac23c
Jacob Cadena <psaajc1420@yahoo.com>
2018-11-02 07:48:17 -0500

    Adding svm model

--- credit_fraud_svm.py
+++ credit_fraud_svm.py
@@ -6,36 +6,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from scipy import interp
-from keras.models import Sequential
-from keras.layers import Dense
+from sklearn.svm import SVC
 from sklearn.metrics import auc
 from sklearn.metrics import roc_curve
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_recall_curve
 from sklearn.model_selection import KFold
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-def create_model():
+def create_model(n_estimator=10):
 
-    # Creating the Neural Network
-    classifier = Sequential()
-
-    # Creating the input layer
-    classifier.add(Dense(units = 100, activation = "relu", kernel_initializer = "uniform", input_dim = 30))
-
-    # Creating all the hidden layers
-    classifier.add(Dense(units = 100, activation = "relu", kernel_initializer = "uniform"))
-    classifier.add(Dense(units = 1, activation = "sigmoid", kernel_initializer = "uniform"))
-
-    # Specify the optimization and loss functions for the 
-    classifier.compile(optimizer = "rmsprop", loss = "binary_crossentropy", metrics = ["accuracy"])
+    classifier = SVC(kernel="linear", probability=True)
 
     return classifier
 
 
-def cross_validate(X, y, n_splits=10, visuals=True):
+def cross_validate(X, y, classifier, n_splits=10, visuals=True):
 
     # Applying K-Fold Cross Validation 
     cv = StratifiedKFold(n_splits=n_splits)
@@ -49,28 +38,36 @@ def cross_validate(X, y, n_splits=10, visuals=True):
         mean_fpr = np.linspace(0, 1, 100)
         tprs = []
         aucs = []
+        f, (ax1, ax2) = plt.subplots(1, 2, figsize=(12,5))
     
     for i, (train, test) in enumerate(cv.split(X, y)):
 
-        classifier = create_model()
-
         # Fitting the Neural Network to the training set
-        classifier.fit(X[train], y[train], batch_size = 200, epochs = 30, verbose=0)
+        classifier.fit(X[train], y[train])
 
-        # Predicting the test set using fitted model
-        y_probs = classifier.predict(X[test], batch_size=200)
-        # y_classes = y_probs.argmax(axis=-1)
-        y_pred = (y_probs > 0.5) 
+        # Predicting the test set per folds
+        y_pred = classifier.predict(X[test])
 
+        # Predicting the test set for positive labels
+        y_probs = classifier.predict_proba(X[test])
+        
         if visuals:
-            fpr, tpr, _ =  roc_curve(y[test], y_probs)
+            fpr, tpr, _ =  roc_curve(y[test], y_probs[:, 1])
             tprs.append(interp(mean_fpr, fpr, tpr))
             tprs[-1][0] = 0.0
             roc_auc = auc(fpr, tpr)
             aucs.append(roc_auc)
-
-            plt.plot(fpr, tpr, lw=1, alpha=0.4,
+            ax1.plot(fpr, tpr, lw=1, alpha=0.4,
                      label="ROC fold {:d} (AUC = {:0.2f})".format(i, roc_auc))
+            
+            precision, recall, _ = precision_recall_curve(y[test], y_probs[:, 1])
+            ax2.step(recall, precision, alpha=0.4, where='post', 
+                     label="PR fold {:d}".format(i))
+            ax2.set_xlabel('Recall')
+            ax2.set_ylabel('Precision')
+            ax2.set_ylim([0.0, 1.05])
+            ax2.set_xlim([0.0, 1.0])
+            ax2.set_title('Precision-Recall: SVM')
 
         _, accuracy, precision, recall, f1_score = metrics(y[test], y_pred, output=False)
 
@@ -80,29 +77,31 @@ def cross_validate(X, y, n_splits=10, visuals=True):
         f1_scores[i] = f1_score
 
     if visuals:
-        plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+        ax1.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
                     label='Chance', alpha=.8)
         mean_tpr = np.mean(tprs, axis=0)
         mean_tpr[-1] = 1.0
         mean_auc = auc(mean_fpr, mean_tpr)
         std_auc = np.std(aucs)
-        plt.plot(mean_fpr, mean_tpr, color='b',
+        ax1.plot(mean_fpr, mean_tpr, color='b',
                 label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
                 lw=2, alpha=.8)
 
         std_tpr = np.std(tprs, axis=0)
         tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
         tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-        plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+        ax1.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
                         label=r'$\pm$ 1 std. dev.')
 
-        plt.xlim([-0.05, 1.05])
-        plt.ylim([-0.05, 1.05])
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
-        plt.title('ROC for Neural Net with Folds')
-        plt.legend(loc="lower right")
-        plt.savefig("ROC_folds.png")
+        ax1.set_xlim([-0.05, 1.05])
+        ax1.set_xlim([-0.05, 1.05])
+        ax1.set_xlabel('False Positive Rate')
+        ax1.set_ylabel('True Positive Rate')
+        ax1.set_title('ROC for SVM with Folds')
+        ax1.legend(loc="lower right")
+        ax2.legend(loc="lower left")
+        
+        plt.savefig("ROC_folds_svm.png")
             
 
     print("Average Accucracy: {:.2f}".format(scores.mean()))
@@ -115,7 +114,6 @@ def metrics(y_test, y_pred, output=True):
 
     # Creating the Confusion Matrix
     cm = confusion_matrix(y_test, y_pred)
-    print(cm)
     tn, fp, fn, tp = cm.ravel()
     accuracy = (tp + tn)/(tn + tp + fn + fp)
     precision = tp/(tp + fp)
@@ -163,7 +161,7 @@ if __name__ == "__main__":
 
     # Fitting the Neural Network to the training set
     start_train = time.time()
-    classifier.fit(X_train, y_train, batch_size = 200, epochs = 30)
+    classifier.fit(X_train, y_train)
     train_time = (time.time() - start_train)
     print("Training time: {}".format(train_time))
 
@@ -173,12 +171,12 @@ if __name__ == "__main__":
     
     metrics(y_test, y_pred, output=True)
 
-    # if sys.argv[1] == "gpu":
-    #     # Running K-Fold Cross validation
-    #     start_kfold = time.time()
-    #     cross_validate(X, y, n_splits=5, visuals=True)
-    #     kfold_time = time.time() - start_kfold
-    #     print("K-Fold training time: {}".format(kfold_time))
+    if sys.argv[1] == "gpu":
+        # Running K-Fold Cross validation
+        start_kfold = time.time()
+        cross_validate(X, y, classifier, n_splits=10, visuals=True)
+        kfold_time = time.time() - start_kfold
+        print("K-Fold training time: {}".format(kfold_time))
 
 
 
